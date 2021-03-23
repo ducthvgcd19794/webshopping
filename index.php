d06ca189bd45b6838cea5286277755264d0d2e3d
ducthvgcd19794 <80742604+ducthvgcd19794@users.noreply.github.com>
2021-03-23 16:32:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BanHang</title>
+    <link rel="stylesheet" type="text/css" href="BanHang.css">
+</head>
+<body>
+    <header> <img src="https://fptshop.com.vn/uploads/images/tin-tuc/78636/Originals/fptshop_an_tet_lon.jpg">
+    </header>
+    <div id="navigation">
+        <nav id="topmenu">
+            <ul>
+              <li><a href="#">Trang Chủ</a></li>
+              <li><a href="#">Thông Tin </a></li>
+              <li><a href="#">Mặt Hàng</a></li>
+              <li><a href="#">Số Lượng</a></li>
+              <li><a href="#">Khác</a></li>
+            </ul>
+          </nav>
+        </div id ="header">
+        <section>
+            <article>
+               <h1>Vsmart Active</h1>
+               <p>Vsmart Active 3 6GB-64GB - CHÍNH HÃNG</p>
+               <img src="https://img.fpt.shop/w_214/h_214/cmpr_10/m_letterbox_ffffff_100/https://fptshop.com.vn/Uploads/Originals/2020/1/16/637147940925292086_Vsmart-active-3-green-dd-bh18.png" alt="">
+               <div class="price">3.390.000 VND</div>
+               <ul>
+                  <li>Trả góp lãi suất 0% áp dụng cho thẻ tín dụng</li>
+                  <li>Trả góp lãi suất ưu đãi thông qua cổng SMP</li>
+               </ul>
+               <button>Purchase it</button>
+            </article>
+            <article>
+               <h1>Laptop Gaming MSI Bravo15</h1>
+               <p>LAPTOP MSI BRAVO15 A4DCR-270VN R5 (4600H/8GB/256GB/AMD RX5300M-3GB/Win10)</p>
+               <img src="https://img.fpt.shop/w_240/h_215/cmpr_10/m_letterbox_ffffff_100/https://fptshop.com.vn/Uploads/Originals/2020/6/11/637274653396615958_msi-bravo-15-%20den-dd.png" alt="">
+               <div class="price">18.000.000VND</div>
+               <ul>
+                  <li>CPU: Ryzen 5</li>
+                  <li>Ram: 8GB </li>
+                  <li>Ổ Cứng: SSD 256 GB</li>
+               </ul>
+               <button>Purchase it</button>
+            </article>
+            <article>
+               <h1>Ipad Air</h1>
+               <p>IPad Air 10.9 2020 Wi-Fi 64GB</p>
+               <img src="https://images.fpt.shop/unsafe/fit-in/214x214/filters:quality(90):fill(white)/fptshop.com.vn/Uploads/Originals/2019/10/11/637063902892576893_ipad-wifi-4g-2019-dd.png" alt="">
+               <div class="price">16.200.000VND</div>
+               <ul>
+                   <li>Trả góp lãi suất 0% áp dụng cho thẻ tín dụng</li>
+                   <li>Trả góp lãi suất ưu đãi thông qua cổng SMP</li>
+               </ul>
+               <button>Purchase it</button>
+            </article>
+         </section>
+</body>
+</html>
